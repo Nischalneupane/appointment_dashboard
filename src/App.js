dcae67889ffa01d6c984177b638809386f17ef5a
Nischal Neupane <pun1sher@Nischals-MacBook-Air.local>
2022-07-08 11:26:57 +0545

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Dashboard from './pages/Dashboard';
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import Appointment from './pages/Appointment';
 function App() {
   return (
+
+  <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Dashboard/>} />
+        <Route path="/appointment" element={<Appointment/>} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
